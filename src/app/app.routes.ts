a378335ec6729e5e3d812faedee4feb197093be9
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-05-29 17:58:06 -0300

    path corregido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 import { OpticavistasComponent } from './componentes/opticavistas/opticavistas.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'optica',
-        pathMatch: 'full'
-    },
-    {
-        path: 'optica',
-        component: OpticavistasComponent
-    }
+  {
+    path: '',
+    component: OpticavistasComponent
+  },
+  {
+    path: 'optica',
+    component: OpticavistasComponent
+  }
 ];
