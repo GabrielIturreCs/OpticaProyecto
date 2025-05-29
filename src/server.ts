e7eafb679196baaaa348d5213d4b96047b090c6f
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-05-29 17:36:26 -0300

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,16 @@ import { render } from "@netlify/angular-runtime/common-engine";
 const commonEngine = new CommonEngine();
 
 export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-  const pathname = new URL(request.url).pathname;
+  try {
+    const pathname = new URL(request.url).pathname;
 
-  if (pathname === "/api/hello") {
-    return Response.json({ message: "Hello from the API" });
+    if (pathname === "/api/hello") {
+      return Response.json({ message: "Hello from the API" });
+    }
+
+    return await render(commonEngine);
+  } catch (error) {
+    console.error('Error en netlifyCommonEngineHandler:', error);
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  return await render(commonEngine);
 }
